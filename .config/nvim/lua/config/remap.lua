0da0348292e4993708f060f0b9b4011189450aef
LucDeCaf <ldecafmeyer@gmail.com>
2025-11-10 15:42:37 +0200

    update nvim config

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -1,23 +1,22 @@
--- terminal
+vim.keymap.set("n", "<leader>rl", "<cmd>source $HOME/.config/nvim/init.lua<CR>", { desc = "Reload Neovim config" })
+
 vim.keymap.set("n", "<leader>Th", "<cmd>vsp term://zsh<CR>i", { desc = "Open terminal left of buffer" })
 vim.keymap.set("n", "<leader>Tj", "<cmd>sp term://zsh<CR><C-w>Ri", { desc = "Open terminal above buffer" })
 vim.keymap.set("n", "<leader>Tk", "<cmd>sp term://zsh<CR>i", { desc = "Open terminal below buffer" })
 vim.keymap.set("n", "<leader>Tl", "<cmd>vsp term://zsh<CR><C-w>Ri", { desc = "Open terminal right of buffer" })
-vim.keymap.set("n", "<leader>/", ":nohl<CR>", { silent = true, desc = "Clear search highlighting" })
+vim.keymap.set("n", "<leader>/", "<cmd>nohl<CR>", { silent = true, desc = "Clear search highlighting" })
+
 vim.keymap.set("t", "<C-Space>", "<C-\\><C-n>", { desc = "Enter normal mode" })
-
--- lsp
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
-
--- file navigation
 vim.keymap.set("n", "<leader><leader>", "<C-^>", { desc = "Jump to previous buffer" })
+vim.keymap.set("n", "<leader>e", "<cmd>Oil<CR>", { desc = "Open Oil file explorer" })
+vim.keymap.set("n", "<leader>ll", "<cmd>Lazy<CR>", { desc = "Open Lazy" })
+vim.keymap.set("n", "<leader>mn", "<cmd>Mason<CR>", { desc = "Open Mason" })
 
--- split navigation
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Focus left pane" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Focus down pane" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Focus up pane" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Focus right pane" })
 
--- remaps
 vim.keymap.set("n", ";", ":")
 vim.keymap.set("n", ":", ";")
--- .config/nvim/lua/config/settings.lua
+++ .config/nvim/lua/config/settings.lua
@@ -1,10 +1,10 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
-
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smarttab = true
+vim.opt.swapfile = false
 
 vim.opt.syntax = "on"
 vim.opt.filetype = "on"
