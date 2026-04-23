37f6e08ae1be49b9946276aaa1e8923c2d469701
LucDeCaf <luc@powersync.com>
2026-04-23 11:00:26 +0200

    Better formatting i hope

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,15 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  opts = {
-    flavour = "mocha",
-    no_italic = true,
-    no_bold = true,
-    compile_path = vim.fn.stdpath "cache" .. "/catppuccin",
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false
-    },
-  },
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	opts = {
+		flavour = "mocha",
+		no_italic = true,
+		no_bold = true,
+		compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false,
+		},
+	},
 }
